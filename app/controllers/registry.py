d4a82e2a20bca1377b11b405c43b18e06e936eea
David Conroy <dconroy@gmail.com>
2021-01-26 10:21:40 -0500

    move match license logic to its own function

--- app/controllers/registry.py
+++ app/controllers/registry.py
@@ -2,9 +2,17 @@ import json
 from controllers.mongo import db, request
 from controllers.utils import hide_email, ordered, format_item
 
+def match_licenses(licensees, licenses_to_check):
+    for licensee in licensees: #for each licensee with the same first/last name
+        for individual_license in licenses_to_check: # and every license type provided in the search
+            for license_held in licensee["license_data"]:
+                if(ordered(individual_license) == ordered(license_held)):
+                    return licensee
+
 def search_licensee(post_data):
     data = []
     possible_matches = 0
+
     #check NRDS, and if matches, confirm first name last name match
     if post_data["nrds"] is not None:
         _licensees = db.registry.find({ "nrds": post_data["nrds"] })
@@ -15,17 +23,17 @@ def search_licensee(post_data):
 
     #If no NRDS search first name, last name, then check licenses
     if possible_matches == 0:
+        matched_by_license = [] #store licensees that are matched by license data
+        licenses_to_check = post_data["license_data"]
         _licensees = db.registry.find({"$and": [{"firstname": post_data["firstname"]}, 
                                                 {"lastname": post_data["lastname"]}]})
-        
-        for licensee in _licensees: #for each licensee with the same first/last name
-            for check_license in post_data["license_data"]: # and every license type provided in the search
-                for license_held in licensee["license_data"]:
-                    if(ordered(check_license) == ordered(license_held)):
-                        possible_matches += 1
-                        data.append(format_item(licensee))
-                        break
-                    
+
+        matched_by_license = match_licenses(_licensees, licenses_to_check)
+
+        if matched_by_license is not None:
+            possible_matches += 1
+            data.append(format_item(matched_by_license))
+                
     data.append({"possible_matches:" : possible_matches})
 
     if possible_matches == 0:
